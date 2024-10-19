a7349e2dbd6d68233b586e1d8ae2a21e63ce79b2
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-10-19 23:09:27 +0530

    feat: add Marker component, update Button component to use Marker, and add prop-types dependency

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,14 @@
 import clsx from "clsx";
-import { Marker } from "./Marker.jsx";
+import Marker from "./Marker";
 
-const Button = ({
-  icon,
-  children,
-  href,
-  containerClassName,
-  onClick,
-  markerFill,
-}) => {
+const Button = ({ icon, children, href, containerClassName, onClick }) => {
   const Inner = () => (
     <>
-      <span>
-        <span>Marker</span>
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+        {" "}
+        <span className="absolute -left-[1px]">
+          <Marker />
+        </span>
         {icon && (
           <img
             src={icon}
@@ -20,12 +16,15 @@ const Button = ({
             className="size-10 mr-5 object-contain z-10"
           />
         )}
-        <span>{children}</span>
-      </span>
+        <span className="relative z-2 font-poppins base-bold text-bold text-p1 uppercase">
+          {children}
+        </span>
 
-      <span className="glow-before glow-after" />
+        <span className="glow-before glow-after" />
+      </span>
     </>
   );
+
   return href ? (
     <a
       className={clsx(
@@ -48,4 +47,5 @@ const Button = ({
     </button>
   );
 };
+
 export default Button;
