a9d489b195aaffce4a7862221f6f97417b6078e2
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-10-19 23:31:03 +0530

    feat: add Faq, Features, and Pricing sections; integrate react-countup for dynamic pricing display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import Faq from "./sections/Faq";
+import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
     </main>
-  )
+  );
 };
 
 export default App;
