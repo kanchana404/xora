c47828d2dc683591b9290d6a296cfe64dfaa84b0
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-10-20 00:09:24 +0530

    feat: add react-slidedown for animated FAQ item transitions in FaqItem component

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,11 +1,13 @@
 import clsx from "clsx";
-import { act } from "react";
 import { useState } from "react";
+import { SlideDown } from "react-slidedown";
+import "react-slidedown/lib/slidedown.css";
 
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
 
   const active = activeId === item.id;
+
   return (
     <div className="relative z-2 mb-16">
       <div
@@ -22,7 +24,7 @@ const FaqItem = ({ item, index }) => {
           <div
             className={clsx(
               "h6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center",
-              active && "max-lg:text-p1"
+              active && "max-lg:text-p1",
             )}
           >
             {item.question}
@@ -32,14 +34,29 @@ const FaqItem = ({ item, index }) => {
         <div
           className={clsx(
             "faq-icon relative flex size-12 items-center justify-center rounded-full border-2 border-s2 shadow-400 transition-all duration-500 group-hover:border-s4",
-            active && "before:bg-p1 after:rotate-0 after:bg-p1"
+            active && "before:bg-p1 after:rotate-0 after:bg-p1",
           )}
         >
           <div className="g4 size-11/12 rounded-full shadow-300" />
         </div>
       </div>
+
+      <SlideDown>
+        {activeId === item.id && (
+          <div className="body-3 px-7 py-3.5">{item.answer}</div>
+        )}
+      </SlideDown>
+
+      <div
+        className={clsx(
+          "g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute",
+          active && "opacity-100",
+        )}
+      >
+        <div className="g4 absolute inset-0.5 -z-1 rounded-3xl" />
+        <div className="absolute left-8 top-0 h-0.5 w-40 bg-p1" />
+      </div>
     </div>
   );
 };
-
 export default FaqItem;
