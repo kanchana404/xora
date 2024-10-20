481044ad12a6ab1fd0987231e497d17fd938cc98
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-10-20 12:50:51 +0530

    feat: add Pricing and Marker components, integrate react-countup, and update App structure

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,6 +1,6 @@
 import { Element } from "react-scroll";
 
-import { details, features } from "../constants/index.jsx";
+import { details, features } from "../../constants/index.jsx";
 import Button from "../components/Button.jsx";
 
 const Features = () => {
